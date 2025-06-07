a141fdea1f2251bef00d11431001c7ab6bac3b48
Vitor-Ricardo-MS <VitorRicardoMS@gmail.com>
2025-06-07 17:40:26 -0300

    Adição das tabelas prota, nao_inteligente, inteligente, inst_ser, inst_prota, npc_dropa, inventario, coletavel e modificador.

--- terramorta.sql
+++ terramorta.sql
@@ -116,3 +116,133 @@ CREATE TABLE evento_dropa(
     FOREIGN KEY (id_item) REFERENCES item_controle(id_item),
     FOREIGN KEY (id_evento) REFERENCES evento(id_evento)
 );
+
+CREATE TABLE prota(
+    id_ser SMALLINT PRIMARY KEY NOT NULL,
+    nome CHAR(50) NOT NULL,
+    hp_base SMALLINT NOT NULL,
+    str_base SMALLINT NOT NULL,
+    dex_base SMALLINT NOT NULL,
+    def_base SMALLINT NOT NULL,
+    res_fogo SMALLINT NOT NULL,
+    res_gelo SMALLINT NOT NULL,
+    res_elet SMALLINT NOT NULL,
+    res_radi SMALLINT NOT NULL,
+    res_cort SMALLINT NOT NULL,
+    res_cont SMALLINT NOT NULL,
+    fome_base SMALLINT NOT NULL,
+    sede_base SMALLINT NOT NULL,
+    carga_base SMALLINT NOT NULL,
+    FOREIGN KEY (id_ser) REFERENCES ser_controle(id_ser)
+);
+
+CREATE TABLE nao_inteligente(
+    id_ser SMALLINT PRIMARY KEY NOT NULL,
+    nome CHAR(50) NOT NULL,
+    hp_base SMALLINT NOT NULL,
+    str_base SMALLINT NOT NULL,
+    dex_base SMALLINT NOT NULL,
+    def_base SMALLINT NOT NULL,
+    res_fogo SMALLINT NOT NULL,
+    res_gelo SMALLINT NOT NULL,
+    res_elet SMALLINT NOT NULL,
+    res_radi SMALLINT NOT NULL,
+    res_cort SMALLINT NOT NULL,
+    res_cont SMALLINT NOT NULL,
+    cabeca BOOL NOT NULL,
+    torso BOOL NOT NULL,
+    maos BOOL NOT NULL,
+    pernas BOOL NOT NULL,
+    pes BOOL NOT NULL,
+    rad_dano SMALLINT NOT NULL,
+    FOREIGN KEY (id_ser) REFERENCES ser_controle(id_ser)
+);
+CREATE TABLE inteligente(
+    id_ser SMALLINT PRIMARY KEY NOT NULL,
+    nome CHAR(50) NOT NULL,
+    hp_base SMALLINT NOT NULL,
+    str_base SMALLINT NOT NULL,
+    dex_base SMALLINT NOT NULL,
+    def_base SMALLINT NOT NULL,
+    res_fogo SMALLINT NOT NULL,
+    res_gelo SMALLINT NOT NULL,
+    res_elet SMALLINT NOT NULL,
+    res_radi SMALLINT NOT NULL,
+    res_cort SMALLINT NOT NULL,
+    res_cont SMALLINT NOT NULL,
+    cabeca BOOL NOT NULL,
+    torso BOOL NOT NULL,
+    maos BOOL NOT NULL,
+    pernas BOOL NOT NULL,
+    pes BOOL NOT NULL,
+    alinhamento SMALLINT NOT NULL,
+    FOREIGN KEY (id_ser) REFERENCES ser_controle(id_ser)
+);
+
+CREATE TABLE inst_ser(
+    id_ser SMALLINT PRIMARY KEY NOT NULL,
+    id_inst SMALLSERIAL PRIMARY KEY NOT NULL,
+    hp_max SMALLINT NOT NULL,
+    hp_atual SMALLINT NOT NULL,
+    str_atual SMALLINT NOT NULL,
+    dex_atual SMALLINT NOT NULL,
+    faccao SMALLINT,
+    def_atual SMALLINT NOT NULL,
+    FOREIGN KEY (id_ser) REFERENCES ser_controle(id_ser)
+    FOREIGN KEY (localizacao) REFERENCES evento(id_evento)
+);
+
+CREATE TABLE inst_prota(
+    id_ser SMALLINT PRIMARY KEY NOT NULL,
+    id_inst SMALLSERIAL PRIMARY KEY NOT NULL,
+    hp_max SMALLINT NOT NULL,
+    hp_atual SMALLINT NOT NULL,
+    str_atual SMALLINT NOT NULL,
+    dex_atual SMALLINT NOT NULL,
+    def_atual SMALLINT NOT NULL,
+    res_fogo_at SMALLINT NOT NULL,
+    res_gelo_at SMALLINT NOT NULL,
+    res_elet_at SMALLINT NOT NULL,
+    res_radi_at SMALLINT NOT NULL,
+    res_cort_at SMALLINT NOT NULL,
+    res_cont_at SMALLINT NOT NULL,
+    fome_max SMALLINT NOT NULL,
+    sede_max SMALLINT NOT NULL,
+    fome_atual SMALLINT NOT NULL,
+    sede_atual SMALLINT NOT NULL,
+    carga_max SMALLINT NOT NULL,
+    carga_atual SMALLINT NOT NULL,
+    faccao SMALLINT,
+    rad_atual SMALLINT NOT NULL,
+    localizacao SMALLINT NOT NULL,
+    FOREIGN KEY (id_ser) REFERENCES prota(id_ser),
+    FOREIGN KEY (faccao) REFERENCES faccao(id_faccao),
+    FOREIGN KEY (localizacao) REFERENCES evento(id_evento)
+);
+CREATE TABLE npc_dropa(
+    id_item SMALLINT PRIMARY KEY NOT NULL,
+    id_ser SMALLINT PRIMARY KEY NOT NULL,
+    chance SMALLINT NOT NULL,
+    FOREIGN KEY (id_item) REFERENCES item_controle(id_item),
+    FOREIGN KEY (id_ser) REFERENCES ser_controle(id_ser)
+);
+
+CREATE TABLE inventario(
+    pos_inv SMALLSERIAL PRIMARY KEY NOT NULL,
+    id_item SMALLINT NOT NULL,
+    quant SMALLINT NOT NULL,
+    FOREIGN KEY (id_item) REFERENCES item_controle(id_item)
+);
+
+CREATE TABLE coletavel(
+    id_item SMALLINT PRIMARY KEY NOT NULL,
+    nome CHAR(50) NOT NULL,
+    FOREIGN KEY (id_item) REFERENCES item_controle(id_item)
+);
+
+CREATE TABLE modificador(
+    id_item SMALLINT PRIMARY KEY NOT NULL,
+    atributo CHAR(50) NOT NULL,
+    valor SMALLINT NOT NULL,
+    FOREIGN KEY (id_item) REFERENCES item_controle(id_item)
+);
