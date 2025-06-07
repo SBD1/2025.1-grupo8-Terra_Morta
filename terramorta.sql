b6f8645d9df36b4405d4b2216da3275601075fa1
VictorPontual <victorpontualgan@gmail.com>
2025-06-07 12:10:43 -0300

    Create terramorta.sql

---
+++ terramorta.sql
@@ -0,0 +1,20 @@
+create table ser_controle(
+	id_ser smallint primary key,
+	tipo char(1)
+)
+create table faccao(
+	id_faccao smallint primary key,
+	tipo char(50)
+)
+create table ser_controle(
+	id_ser smallint primary key,
+	tipo char(1)
+)
+create table ser_controle(
+	id_ser smallint primary key,
+	tipo char(1)
+)
+create table ser_controle(
+	id_ser smallint primary key,
+	tipo char(1)
+);
