180f570904df418e6a3f4e52af0dc8431b2c45c6
VictorPontual <victorpontualgan@gmail.com>
2025-06-07 12:30:44 -0300

    Update terramorta.sql

--- terramorta.sql
+++ terramorta.sql
@@ -1,20 +1,33 @@
 create table ser_controle(
-	id_ser smallint primary key,
-	tipo char(1)
+	id_ser smallserial primary key not null,
+	tipo char(1) not null
 )
 create table faccao(
-	id_faccao smallint primary key,
-	tipo char(50)
+	id_faccao smallserial primary key not null,
+	nome_faccao char(50) not null
 )
-create table ser_controle(
+create table mutacao_atual(
 	id_ser smallint primary key,
-	tipo char(1)
+	cabeca smallint,
+	torso smallint,
+	maos smallint,
+	pernas smallint,
+	pes smallint,
+	FOREIGN KEY (id_ser) REFERENCES ser_controle(id_ser),
+	FOREIGN KEY (cabeca) REFERENCES mutacao(id_mutacao),
+	FOREIGN KEY (torso) REFERENCES mutacao(id_mutacao),
+	FOREIGN KEY (maos) REFERENCES mutacao(id_mutacao),
+	FOREIGN KEY (pernas) REFERENCES mutacao(id_mutacao),
+	FOREIGN KEY (pes) REFERENCES mutacao(id_mutacao)
 )
-create table ser_controle(
-	id_ser smallint primary key,
-	tipo char(1)
+create table mutacao(
+	id_mutacao smallint primary key,
+	nome char(50) not null,
+	nivel smallint not null,
+	parte_corpo char(4),
+	FOREIGN KEY (id_mutacao) REFERENCES item_controle(id_item),
 )
-create table ser_controle(
-	id_ser smallint primary key,
+create table item_controle(
+	id_item smallserial primary key not null,
 	tipo char(1)
 );
