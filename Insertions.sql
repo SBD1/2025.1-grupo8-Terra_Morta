50761b62b3e215b8a337015ea63a610241ed93d1
VictorPontual <victorpontualgan@gmail.com>
2025-06-08 18:29:55 -0300

    inserts and tables
    Co-Authored-By: EnzoEmir <164296530+EnzoEmir@users.noreply.github.com>
    Co-Authored-By: Vitor-Ricardo-MS <92763948+Vitor-Ricardo-MS@users.noreply.github.com>

--- Insertions.sql
+++ Insertions.sql
@@ -1,4 +1,5 @@
 INSERT INTO ponto_de_interesse (nome, custo, nivel_rad) VALUES
+('Base Central', 0, 0.00),
 ('Refinaria Abandonada', 120, 3.75),
 ('Estação de Trem Velha', 60, 1.20),
 ('Hospital Subterrâneo', 200, 4.90),
@@ -9,3 +10,24 @@ INSERT INTO ponto_de_interesse (nome, custo, nivel_rad) VALUES
 ('Cidade Fantasma', 100, 2.65),
 ('Parque Industrial', 140, 3.30),
 ('Estação de Tratamento de Água', 90, 1.50);
+
+INSERT INTO item_controle (id_item, tipo) VALUES
+('C'),
+('C'),
+('C');
+
+INSERT INTO coletavel (id_item, nome) VALUES
+('1','Madeira'),
+('2','Ferro'),
+('3','Núcleo Radioativo Fundido');
+
+INSERT INTO instalacao_base (nome, nivel, id_item, qtd) VALUES
+('Acampamento Improvisado', 0, NULL, NULL),
+('Refúgio de Madeira', 1, 1, 5),
+('Fortificação de Madeira', 2, 1, 15),
+('Depósito de Ferro Reforçado', 3, 2, 5),
+('Complexo Técnico Avançado', 4, 2, 15),
+('Santuário do Núcleo', 5, 3, 3);
+
+INSERT INTO base (id_pi, nome, id_instalacao) VALUES 
+(1, 'Base Central', 0);
--- terramorta.sql
+++ terramorta.sql
@@ -1,3 +1,4 @@
+-- SQLBook: Code
 create table ser_controle(
 	id_ser smallserial primary key not null,
 	tipo char(1) not null
@@ -8,6 +9,7 @@ create table faccao(
 	nome_faccao char(50) not null
 );
 
+<<<<<<< Updated upstream
 create table item_controle(
 	id_item smallserial primary key not null,
 	tipo char(1)
@@ -124,6 +126,8 @@ CREATE TABLE evento_dropa(
     FOREIGN KEY (id_evento) REFERENCES evento(id_evento)
 );
 
+=======
+>>>>>>> Stashed changes
 CREATE TABLE prota(
     id_ser SMALLINT PRIMARY KEY NOT NULL,
     nome CHAR(50) NOT NULL,
@@ -156,11 +160,11 @@ CREATE TABLE nao_inteligente(
     res_radi SMALLINT NOT NULL,
     res_cort SMALLINT NOT NULL,
     res_cont SMALLINT NOT NULL,
-    cabeca BOOL NOT NULL,
-    torso BOOL NOT NULL,
-    maos BOOL NOT NULL,
-    pernas BOOL NOT NULL,
-    pes BOOL NOT NULL,
+    cabeca BOOLEAN NOT NULL,
+    torso BOOLEAN NOT NULL,
+    maos BOOLEAN NOT NULL,
+    pernas BOOLEAN NOT NULL,
+    pes BOOLEAN NOT NULL,
     rad_dano SMALLINT NOT NULL,
     FOREIGN KEY (id_ser) REFERENCES ser_controle(id_ser)
 );
@@ -178,13 +182,154 @@ CREATE TABLE inteligente(
     res_radi SMALLINT NOT NULL,
     res_cort SMALLINT NOT NULL,
     res_cont SMALLINT NOT NULL,
-    cabeca BOOL NOT NULL,
-    torso BOOL NOT NULL,
-    maos BOOL NOT NULL,
-    pernas BOOL NOT NULL,
-    pes BOOL NOT NULL,
+    cabeca BOOLEAN NOT NULL,
+    torso BOOLEAN NOT NULL,
+    maos BOOLEAN NOT NULL,
+    pernas BOOLEAN NOT NULL,
+    pes BOOLEAN NOT NULL,
     alinhamento SMALLINT NOT NULL,
+    FOREIGN KEY (alinhamento) REFERENCES faccao(id_faccao),
     FOREIGN KEY (id_ser) REFERENCES ser_controle(id_ser)
+);
+
+create table item_controle(
+	id_item smallserial primary key not null,
+	tipo char(1)
+);
+
+CREATE TABLE inventario(
+    pos_inv SMALLSERIAL PRIMARY KEY NOT NULL,
+    id_item SMALLINT NOT NULL,
+    quant SMALLINT NOT NULL,
+    FOREIGN KEY (id_item) REFERENCES item_controle(id_item)
+);
+
+CREATE TABLE npc_dropa(
+    id_item SMALLINT NOT NULL,
+    id_ser SMALLINT NOT NULL,
+    chance SMALLINT NOT NULL,
+    PRIMARY KEY(id_item,id_ser),
+    FOREIGN KEY (id_item) REFERENCES item_controle(id_item),
+    FOREIGN KEY (id_ser) REFERENCES ser_controle(id_ser)
+);
+
+CREATE TABLE coletavel(
+    id_item SMALLINT PRIMARY KEY NOT NULL,
+    nome CHAR(50) NOT NULL,
+    FOREIGN KEY (id_item) REFERENCES item_controle(id_item)
+);
+
+create table equipamento(
+	id_equip smallint primary key not null,
+	nome char(50) not null,
+	nivel smallint not null,
+	parte_corpo char(4),
+	FOREIGN KEY (id_equip) REFERENCES item_controle(id_item)
+);
+
+create table equipamento_atual(
+	id_ser smallint primary key not null,
+	cabeca smallint,
+	torso smallint,
+	maos smallint,
+	pernas smallint,
+	pes smallint,
+	FOREIGN KEY (id_ser) REFERENCES ser_controle(id_ser),
+	FOREIGN KEY (cabeca) REFERENCES equipamento(id_equip),
+	FOREIGN KEY (torso) REFERENCES equipamento(id_equip),
+	FOREIGN KEY (maos) REFERENCES equipamento(id_equip),
+	FOREIGN KEY (pernas) REFERENCES equipamento(id_equip),
+	FOREIGN KEY (pes) REFERENCES equipamento(id_equip)
+);
+
+create table mutacao(
+	id_mutacao smallint primary key not null,
+	nome char(50) not null,
+	nivel smallint not null,
+	parte_corpo char(4),
+	FOREIGN KEY (id_mutacao) REFERENCES item_controle(id_item)
+);
+
+create table mutacao_atual(
+	id_ser smallint primary key not null,
+	cabeca smallint,
+	torso smallint,
+	maos smallint,
+	pernas smallint,
+	pes smallint,
+	FOREIGN KEY (id_ser) REFERENCES ser_controle(id_ser),
+	FOREIGN KEY (cabeca) REFERENCES mutacao(id_mutacao),
+	FOREIGN KEY (torso) REFERENCES mutacao(id_mutacao),
+	FOREIGN KEY (maos) REFERENCES mutacao(id_mutacao),
+	FOREIGN KEY (pernas) REFERENCES mutacao(id_mutacao),
+	FOREIGN KEY (pes) REFERENCES mutacao(id_mutacao)
+);
+
+CREATE TABLE modificador(
+    id_item SMALLINT PRIMARY KEY NOT NULL,
+    atributo CHAR(50) NOT NULL,
+    valor SMALLINT NOT NULL,
+    FOREIGN KEY (id_item) REFERENCES item_controle(id_item)
+);
+
+CREATE TABLE evento (
+    id_evento SMALLSERIAL PRIMARY KEY,
+    max_ocorrencia SMALLINT 
+);
+
+CREATE TABLE evento_dropa(
+    id_item SMALLINT,
+    id_evento SMALLINT,
+    PRIMARY KEY(id_item,id_evento),
+    FOREIGN KEY (id_item) REFERENCES item_controle(id_item),
+    FOREIGN KEY (id_evento) REFERENCES evento(id_evento)
+);
+
+CREATE TABLE ponto_de_interesse (
+    id_pi SMALLSERIAL PRIMARY KEY,
+    nome VARCHAR(50),
+    custo SMALLINT, 
+    nivel_rad DECIMAL(5,2) 
+);
+
+CREATE TABLE requisitos (
+    id_evento SMALLINT PRIMARY KEY,
+    req VARCHAR(50), 
+    status BOOLEAN, 
+    FOREIGN KEY (id_evento) REFERENCES evento(id_evento)
+);
+
+CREATE TABLE ocorre(
+    id_evento smallint,
+    id_pi smallint ,
+    PRIMARY KEY(id_evento,id_pi),
+    FOREIGN KEY (id_evento) REFERENCES evento(id_evento),
+    FOREIGN KEY (id_pi) REFERENCES ponto_de_interesse(id_pi)
+);
+
+CREATE TABLE missao (
+    id_evento SMALLINT PRIMARY KEY,
+    status CHAR(1) CHECK (status IN ('C', 'A', 'F', 'N')),
+    prox SMALLINT, 
+    FOREIGN KEY (id_evento) REFERENCES evento(id_evento),
+    FOREIGN KEY (prox) REFERENCES evento(id_evento)
+);
+
+CREATE TABLE instalacao_base (
+    id_instalacao SMALLSERIAL PRIMARY KEY,
+    nome VARCHAR(50),
+    nivel SMALLINT,
+    item SMALLINT,
+    quantidade SMALLINT,
+    FOREIGN KEY (item) REFERENCES item_controle(id_item)
+);
+
+CREATE TABLE base (
+    id_pi SMALLINT PRIMARY KEY,
+    nome VARCHAR(50),
+    id_instalacao SMALLINT,
+    FOREIGN KEY (id_pi) REFERENCES ponto_de_interesse(id_pi),
+    FOREIGN KEY (id_instalacao) REFERENCES instalacao_base(id_instalacao)
 );
 
 CREATE TABLE inst_ser(
@@ -228,27 +373,4 @@ CREATE TABLE inst_prota(
     FOREIGN KEY (id_ser) REFERENCES prota(id_ser),
     FOREIGN KEY (faccao) REFERENCES faccao(id_faccao),
     FOREIGN KEY (localizacao) REFERENCES evento(id_evento)
-);
-
-CREATE TABLE npc_dropa(
-    id_item SMALLINT NOT NULL,
-    id_ser SMALLINT NOT NULL,
-    chance SMALLINT NOT NULL,
-    PRIMARY KEY(id_item,id_ser),
-    FOREIGN KEY (id_item) REFERENCES item_controle(id_item),
-    FOREIGN KEY (id_ser) REFERENCES ser_controle(id_ser)
-);
-
-CREATE TABLE inventario(
-    pos_inv SMALLSERIAL PRIMARY KEY NOT NULL,
-    id_item SMALLINT NOT NULL,
-    quant SMALLINT NOT NULL,
-    FOREIGN KEY (id_item) REFERENCES item_controle(id_item)
-);
-
-CREATE TABLE modificador(
-    id_item SMALLINT PRIMARY KEY NOT NULL,
-    atributo CHAR(50) NOT NULL,
-    valor SMALLINT NOT NULL,
-    FOREIGN KEY (id_item) REFERENCES item_controle(id_item)
 );
