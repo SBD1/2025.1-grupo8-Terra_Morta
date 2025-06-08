0e54c84e5fb31a36e21ad01225747b926e2c2e6a
VictorPontual <victorpontualgan@gmail.com>
2025-06-08 17:15:28 -0300

    Create Insertions.sql

---
+++ Insertions.sql
@@ -0,0 +1,11 @@
+INSERT INTO ponto_de_interesse (nome, custo, nivel_rad) VALUES
+('Refinaria Abandonada', 120, 3.75),
+('Estação de Trem Velha', 60, 1.20),
+('Hospital Subterrâneo', 200, 4.90),
+('Zona de Comércio Queimada', 80, 2.10),
+('Laboratório Químico', 150, 5.00),
+('Aeroporto Militar', 180, 3.95),
+('Bunker da Resistência', 220, 1.85),
+('Cidade Fantasma', 100, 2.65),
+('Parque Industrial', 140, 3.30),
+('Estação de Tratamento de Água', 90, 1.50);
