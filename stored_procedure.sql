c8704f1d07166f175aabe4a6c49b190aabdeb9ab
EnzoEmir <164296530+EnzoEmir@users.noreply.github.com>
2025-07-01 18:38:23 -0300

    feat: Adicionando o criar_encontro

---
+++ stored_procedure.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION criar_encontro(
+    p_id_inimigo INT,
+    p_quantidade INT,
+    p_local INT,
+    p_max_ocorrencia INT DEFAULT 1,
+    p_prioridade CHAR(1) DEFAULT 'A',
+    p_probabilidade CHAR(3) DEFAULT '100'
+)
+RETURNS INT AS $$
+DECLARE
+    v_id_evento INT;
+BEGIN
+    -- Cria o evento do tipo ENCONTRO
+    INSERT INTO evento (max_ocorrencia, prioridade, probabilidade, tipo)
+    VALUES (p_max_ocorrencia, p_prioridade, p_probabilidade, 'ENCONTRO')
+    RETURNING id_evento INTO v_id_evento;
+
+    -- Cria o encontro vinculado ao evento
+    INSERT INTO encontro (id_evento, id_inimigo, quantidade, local)
+    VALUES (v_id_evento, p_id_inimigo, p_quantidade, p_local);
+
+    RETURN v_id_evento;
+END;
+$$ LANGUAGE plpgsql;
