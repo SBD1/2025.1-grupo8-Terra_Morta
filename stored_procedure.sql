a11abf67667d7b3b3c0629941bf61dda36c69f34
EnzoEmir <164296530+EnzoEmir@users.noreply.github.com>
2025-07-04 17:29:44 -0300

    feat: Stored Procedure para crirar acontecimentos mundo

--- stored_procedure.sql
+++ stored_procedure.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE FUNCTION criar_encontro(
     p_prioridade CHAR(1) DEFAULT '1',
     p_probabilidade CHAR(3) DEFAULT '50'
 )
-RETURNS INT AS $$
+RETURNS INT AS $criar_encontro$
 DECLARE
     v_id_evento INT;
 BEGIN
@@ -25,4 +25,35 @@ BEGIN
 
     RETURN v_id_evento;
 END;
-$$ LANGUAGE plpgsql;
+$criar_encontro$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION criar_acontecimento_mundo(
+    p_atributo INT,
+    p_valor INT,
+    p_texto VARCHAR,
+    p_local INT,
+    p_max_ocorrencia INT DEFAULT 1,
+    p_prioridade CHAR(1) DEFAULT '1',
+    p_probabilidade CHAR(3) DEFAULT '50'
+)
+RETURNS INT AS $criar_acontecimento$
+DECLARE
+    v_id_evento INT;
+BEGIN
+    -- Cria o evento do tipo ACONTECIMENTO MUNDO
+    INSERT INTO evento (max_ocorrencia, prioridade, probabilidade, tipo)
+    VALUES (p_max_ocorrencia, p_prioridade, p_probabilidade, 'ACONTECIMENTO MUNDO')
+    RETURNING id_evento INTO v_id_evento;
+
+    -- Cria o acontecimento_mundo vinculado ao evento
+    INSERT INTO acontecimento_mundo (id_evento, atributo, valor, texto)
+    VALUES (v_id_evento, p_atributo, p_valor, p_texto);
+
+    -- Relaciona o evento ao ponto de interesse
+    INSERT INTO ocorre (id_evento, id_pi)
+    VALUES (v_id_evento, p_local);
+
+    RETURN v_id_evento;
+END;
+$criar_acontecimento$ LANGUAGE plpgsql;
